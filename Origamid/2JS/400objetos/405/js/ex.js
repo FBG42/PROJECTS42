c18b722877bd351dfe2f98a30d6f64cb1d8bae98
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-06-20 19:33:23 -0300

    Corrigindo ex com exemplos do professor

--- Origamid/2JS/400objetos/405/js/ex.js
+++ Origamid/2JS/400objetos/405/js/ex.js
@@ -5,15 +5,23 @@ console.log(ale);
 
 // Retorne o maior número da lista abaixo
 let numeros = "4, 5, 20, 8, 9";
-let numeroArray = [];
+// let numeroArray = [];
 
-numeros = numeros.split(", ");
-console.log(numeros);
-numeros.forEach((numero) => {
-  numero = Number.parseInt(numero);
-  numeroArray.push(numero);
-});
-console.log(Math.max(...numeroArray));
+//Minha solução
+// numeros = numeros.split(", ");
+// console.log(numeros);
+// numeros.forEach((numero) => {
+//   numero = Number.parseInt(numero);
+//   numeroArray.push(numero);
+// });
+// console.log(Math.max(...numeroArray));
+
+
+//Solução do professor
+const arrayNumeros = numeros.split(', ');
+const numeroMaximo = Math.max(...arrayNumeros);
+console.log(numeroMaximo);
+
 
 
 // Crie uma função para limpar os preços
@@ -22,21 +30,38 @@ console.log(Math.max(...numeroArray));
 
 const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
 
-let total = 0;
-function limpar(array) {
-  let itemAtribuido;
-  array.forEach((item) => {
-    if (item.includes("R$")) {
-      itemAtribuido = item.replace("R$", "");
-    } else {
-      itemAtribuido = item.replace("r$", "");
-    }
-    itemAtribuido = itemAtribuido.replaceAll(",", ".");
-    itemAtribuido = Math.round(itemAtribuido);
-    total += itemAtribuido;
-    return total;
-  });
+
+// Minha solução
+// let total = 0;
+// function limpar(array) {
+//   let itemAtribuido;
+//   array.forEach((item) => {
+//     if (item.includes("R$")) {
+//       itemAtribuido = item.replace("R$", "");
+//     } else {
+//       itemAtribuido = item.replace("r$", "");
+//     }
+//     itemAtribuido = itemAtribuido.replaceAll(",", ".");
+//     itemAtribuido = Math.round(itemAtribuido);
+//     total += itemAtribuido;
+//     return total;
+//   });
+// }
+
+// limpar(listaPrecos);
+// console.log(total)
+
+// Solução do professor
+function limparPreco(preco) {
+  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+  preco = +preco.toFixed(2)
+  return preco
 }
 
-limpar(listaPrecos);
-console.log(total)
+let soma = 0;
+listaPrecos.forEach((item) => {
+  soma += limparPreco(item)
+})
+
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+
