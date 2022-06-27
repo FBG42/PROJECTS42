5d087a897e46bf44ba7c316b5a6a52d8afe6a7ad
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-06-27 13:58:43 -0300

    Aula do dia 27/06 - Almoço

--- Origamid/2JS/400objetos/407/js/script.js
+++ Origamid/2JS/400objetos/407/js/script.js
@@ -5,7 +5,7 @@
 //     console.log(item, index, array);
 // })
 
-// console.log(carros); 
+// console.log(carros);
 
 // //
 
@@ -17,10 +17,7 @@
 //     item.classList.add('ativa');
 // });
 
-
 // const carros = ['Ford', 'Fiat', 'Honda'];
-
-
 
 // carros.forEach((item, index, array) => {
 //     array[index] = 'Carro ' + item;
@@ -41,24 +38,7 @@
 // console.log(numerosX2);
 // console.log(novaArray);
 
-// const aulas = [
-//     {
-//       nome: 'HTML 1',
-//       min: 15
-//     },
-//     {
-//       nome: 'HTML 2',
-//       min: 10
-//     },
-//     {
-//       nome: 'CSS 1',
-//       min: 20
-//     },
-//     {
-//       nome: 'JS 1',
-//       min: 25
-//     },
-//   ];
+//
 
 // const tempoAulas = aulas.map(aula => aula.min);
 
@@ -66,24 +46,110 @@
 
 // const nomeAulas2 = function(aula) {
 //     aula.nome;
-// } 
+// }
 
 // const arrayNomeAulas = aulas.map(nomeAulas);
 
-const aulas = [10, 25, 30];
+// const aulas = [10, 25, 30];
 
-const reduceAulas = aulas.reduce((acumulador, item) => {
-    console.log(acumulador, item);
-    return item;
-}, 0);
+// const reduceAulas = aulas.reduce((acumulador, item) => {
+//     console.log(acumulador, item);
+//     return item;
+// }, 0);
 
-console.log(reduceAulas)
+// console.log(reduceAulas)
 
+// const numeros = [10, 25, 30, 3, 54, 33, 22];
 
-const numeros = [10, 25, 30, 3, 54, 33, 22];
+// const maiorNumero = numeros.reduce((anterior, atual) => {
+//     return anterior > atual ? anterior : atual;
+// }, 0);
 
-const maiorNumero = numeros.reduce((anterior, atual) => {
-    return anterior > atual ? anterior : atual;
-}, 0);
+// console.log(maiorNumero);
 
-console.log(maiorNumero);
+// const aulas = [
+//         {
+//           nome: 'HTML 1',
+//           min: 15
+//         },
+//         {
+//           nome: 'HTML 2',
+//           min: 10
+//         },
+//         {
+//           nome: 'CSS 1',
+//           min: 20
+//         },
+//         {
+//           nome: 'JS 1',
+//           min: 25
+//         },
+//       ];
+
+// const listaAulas = aulas.reduce((acumulador, aula, index) => {
+//     console.log(aula.nome)
+//     acumulador[index] = aula.nome;
+//     return acumulador
+// }, [])
+
+// const frutas = ['Banana', 'Pêra', 'Uva'];
+
+// const temUva = frutas.some((item) => {
+//     console.log(item);
+//     return item === 'Uva';
+// });
+
+// const every = frutas.every((fruta) => {
+//     console.log(fruta);
+//     return fruta
+// });
+
+// console.log(every);
+
+// const numeros = [6, 43, 22, 88, 101, 29];
+
+// const maiorQue3 = numeros.every(n => n >= 6);
+
+// console.log(maiorQue3);
+
+// const frutas = ['Banana', 'Pêra', 'Uva'];
+
+// const indexUva = frutas.findIndex((fruta) => {
+//     return fruta === 'Uva';
+// });
+
+// console.log(indexUva)
+
+// const frutas = ['Banana', null, undefined, 'Pêra', 0, 'Uva'];
+
+// const arrayFrutas = frutas.filter((fruta) => {
+//     console.log(fruta);
+//     return fruta;
+// });
+
+// console.log(arrayFrutas);
+
+const aulas = [
+  {
+    nome: "HTML 1",
+    min: 15,
+  },
+  {
+    nome: "HTML 2",
+    min: 10,
+  },
+  {
+    nome: "CSS 1",
+    min: 20,
+  },
+  {
+    nome: "JS 1",
+    min: 25,
+  },
+];
+
+const maiores15 = aulas.filter((aula) => {
+    return aula.min >= 15;
+});
+
+console.log(maiores15)
