c4ea814c6621402a29a6ddf3890ab18e70e42f11
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-07-08 17:46:54 -0300

    Aula do dia 08/07 - tarde

---
+++ Origamid/2JS/500efeitosNoDom/502/js/modules/tabnav.js
@@ -0,0 +1,28 @@
+// Navegação em Tab nas fotos dos animais
+export default function initTabNav() {
+    const tabMenu = document.querySelectorAll("[data-js='tabmenu'] li");
+    const tabContent = document.querySelectorAll('[data-js="tabcontent"] section');
+  
+    console.log(tabMenu)
+  
+    if (tabMenu.length && tabContent.length) {
+      tabContent[0].classList.add("ativo");
+  
+      function activeTab(index) {
+        tabContent.forEach((section) => {
+          section.classList.remove("ativo", tabContent[index].dataset.anime);
+        });
+        console.log();
+        tabContent[index].classList.add("ativo", tabContent[index].dataset.anime);
+      }
+  
+      tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener("click", () => {
+          activeTab(index);
+        });
+      });
+    }
+  }
+  initTabNav();
+  
+  
