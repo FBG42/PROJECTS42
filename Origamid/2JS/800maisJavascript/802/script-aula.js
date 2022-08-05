ba6e88366aeb477dabe734b7c1f1158091422d67
fbgrigolo <fbc.grigolo@gmail.com>
2022-08-05 16:37:35 -0300

    Aulas de 05/07 - tarde

---
+++ Origamid/2JS/800maisJavascript/802/script-aula.js
@@ -0,0 +1,34 @@
+// function createButton(text) {
+// 	const numeroSecreto = '1092381029';
+
+// 	function element() {
+// 		const buttonElement = document.createElement('button');
+// 		buttonElement.innerText = text;
+// 		return buttonElement;
+// 	}
+
+// 	return Object.freeze({
+// 		text,
+// 		element,
+// 	});
+// }
+
+// const btnComprar = createButton('Comprar');
+// const btnVender = createButton('Vender');
+
+// function Pessoa(nome) {
+// 	if (!(this instanceof Pessoa)) 
+// 		return new Pessoa(nome);
+// 	this.nome = nome;
+// }
+
+// Pessoa.prototype.andar = function () {
+// 	return `${this.nome} andou`;
+// };
+
+// const designer = Pessoa('André');
+
+// console.log(designer);
+
+
+
