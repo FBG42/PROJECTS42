b6fee4c8cc9d4456b45910da32a9896342a1d237
Fabrício <fbc.grigolo@gmail.com>
2022-09-21 17:50:48 -0300

    Aula do 21/09 - tarde

---
+++ Origamid/3Wordpress/rest/wp-content/themes/rest/footer.php
@@ -0,0 +1,18 @@
+<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+
+      <!-- Início Footer Wordpress -->
+      <?php wp_footer(); ?>
+      <!-- Final footer wordpress -->
+	</body>
+</html>
+
