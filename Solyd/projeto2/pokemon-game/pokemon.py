2a8364981ed2c27140a111590cd0d46418d788d8
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-12-08 16:57:48 -0300

    Aulas de 08/12 no mate

--- Solyd/projeto2/pokemon-game/pokemon.py
+++ Solyd/projeto2/pokemon-game/pokemon.py
@@ -1,12 +1,17 @@
 class Pokemon:
    def __init__(self, tipo, especie):
-      self.tipo = tipo;
-      self.especie = especie;
+      self.tipo = tipo
+      self.especie = especie
+   
+   def __str__(self):
+      return f"{self.especie} ({self.tipo})"
 
+   def atacar(self, pokemon):
+      print(f"{self.especie} atacou {pokemon.especie}")
+   
 
 meu_pokemon = Pokemon("fogo", "charmander")
-pokemon_amigo = Pokemon("eletrico", "pikachu")
+pokemon_amigo = Pokemon("elétrico", "pikachu")
 
-print(meu_pokemon, meu_pokemon.tipo, meu_pokemon.especie)
-
-print(pokemon_amigo, pokemon_amigo.tipo, pokemon_amigo.especie)
+meu_pokemon.atacar(pokemon_amigo)
+pokemon_amigo.atacar(meu_pokemon)
