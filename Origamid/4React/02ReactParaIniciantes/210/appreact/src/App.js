ddffd536a0598884854f856564189c2087c7f42a
fbgrigolo <fbc.grigolo@gmail.com>
2023-01-24 17:57:04 -0300

    Aulas de 24/01 - tarde

---
+++ Origamid/4React/02ReactParaIniciantes/210/appreact/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Footer from "./Footer";
+import Form from "./Form/Form";
+import Header from "./Header";
+
+const Teste = () => {
+  const active = false;
+
+  if (active) {
+    return <p>Teste</p>
+  }
+  else {
+    return null;
+  }
+};
+
+function App() {
+    return (
+      <>
+        <Teste />
+        <Header />
+        <Form />
+        <Footer />
+      </>
+    );
+};
+
+export default App;
