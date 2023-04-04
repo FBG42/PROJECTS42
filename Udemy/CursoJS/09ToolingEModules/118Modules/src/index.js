a2bba02b46476c4e63f75c2554a10c24316eff40
fbgrigolo <fbc.grigolo@gmail.com>
2023-04-04 14:03:03 -0300

    Aulas de 04/04 - almoço

---
+++ Udemy/CursoJS/09ToolingEModules/118Modules/src/index.js
@@ -0,0 +1,7 @@
+import multiplica, { nome as nome2, sobrenome, idade, soma } from './modulo1';
+
+const nome = 'João';
+
+console.log(nome, nome2, sobrenome, idade);
+console.log(soma(21, 21))
+console.log(multiplica(21, 2))
