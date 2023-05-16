4c2e3dd0235e725996980df71c90c70b401ee719
fbgrigolo <fbc.grigolo@gmail.com>
2023-05-16 17:53:35 -0300

    Aula de 16/05

--- Udemy/CursoJS/14ReactComponentes/listatarefas/.eslintrc.js
+++ Udemy/CursoJS/14ReactComponentes/listatarefas/.eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'import/no-duplicates': 0,
     'class-methods-use-this': 'warn',
     'react/no-unused-state': 0,
+    'react/forbid-prop-types': 0,
   },
 };
---
+++ Udemy/CursoJS/15ReactRedux/react-base/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  overrides: [],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        parser: 'flow',
+      },
+    ],
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
