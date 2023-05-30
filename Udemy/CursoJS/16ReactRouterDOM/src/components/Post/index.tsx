0126b09885a97dba4992295915b09d4254511844
fbgrigolo <fbc.grigolo@gmail.com>
2023-05-30 18:42:30 -0300

    Aulas de 30/05

---
+++ Udemy/CursoJS/16ReactRouterDOM/src/components/Post/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export const Post = () => {
+	return (
+		<div>
+			<h1>Post</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut iure, quisquam repellat ipsum maiores harum! Quibusdam, veniam? Aut delectus incidunt, eius consectetur odio iusto asperiores, ipsa voluptatem, soluta quia commodi!
+			</p>
+		</div>
+	);
+};
