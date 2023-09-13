053a8714860505baf4d2c3b8e8da89471b877cf8
Fabrício <fbc.grigolo@gmail.com>
2023-09-13 19:50:47 -0300

    Aulas de 13/09

---
+++ Solyd-2023/05-modulosBonus/03-apiPaises/paises.py
@@ -0,0 +1,58 @@
+import json
+
+import requests
+
+URL_ALL = "https://restcountries.com/v2/all"
+URL_NAME = "https://restcountries.com/v2/name/"
+
+def requisicao(url):
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            return resposta.text
+    except:
+        print("Erro ao fazer requsição em:", url)
+
+def parsing(texto_da_resposta):
+    try:
+        return json.loads(texto_da_resposta)
+    except:
+        print("Erro ao fazer parsing")
+
+def contagem_de_paises(lista_de_paises):
+    return len(lista_de_paises)
+
+def listar_paises(lista_de_paises):
+    for pais in lista_de_paises:
+        print(pais['name'])
+
+def mostrar_populacao(nome_do_pais):
+    resposta = requisicao(URL_NAME + nome_do_pais)
+    if resposta:
+        lista_de_paises = parsing(resposta)
+
+    if lista_de_paises:
+        for pais in lista_de_paises:
+            print(f"{pais['name']}: {pais['population']}")
+    else:
+        print("Pais não encontrado")
+
+def mostrar_moedas(nome_do_pais):
+    resposta = requisicao(URL_NAME + nome_do_pais)
+    if resposta:
+        lista_de_paises = parsing(resposta)
+
+    if lista_de_paises:
+        for pais in lista_de_paises:
+            print("Moedas do", pais["name"])
+            moedas = pais["currencies"]
+            for moeda in moedas:
+                print(f"{moeda['name']} - {moeda['code']}")
+
+    else:
+        print("Pais não encontrado")
+
+
+if __name__ == "__main__":
+    mostrar_moedas('bra')
+    #mostrar_populacao('brazil')
