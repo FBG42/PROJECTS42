a21a90a44293c5c2ff1189eaf055874441dd9c50
fbgrigolo <fbc.grigolo@gmail.com>
2023-09-14 21:04:51 -0300

    Aulas de 14/09

---
+++ Udemy/CursoC#/09enumeracaoEComposicao/124enumeracoes/enumerations/enumerations/entities/order.cs
@@ -0,0 +1,21 @@
+﻿using enumerations.entities.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumerations.entities
+{
+    class order
+    {
+        public int id { get; set; }
+        public DateTime moment { get; set; }
+        public orderStatus status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id}, {moment}, {status}";
+        }
+    }
+}
