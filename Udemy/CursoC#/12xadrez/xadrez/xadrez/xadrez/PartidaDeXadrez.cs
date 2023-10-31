10ce9ad31f222d9127e00a7bdab4475d5fc66859
fbgrigolo <fbc.grigolo@gmail.com>
2023-10-31 14:33:17 -0300

    Aulas de 31/10

--- Udemy/CursoC#/12xadrez/xadrez/xadrez/xadrez/PartidaDeXadrez.cs
+++ Udemy/CursoC#/12xadrez/xadrez/xadrez/xadrez/PartidaDeXadrez.cs
@@ -143,6 +143,22 @@ namespace xadrez
                 throw new TabuleiroExpection("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaEspecial promoção
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(JogadorAtual)))
             {
                 xeque = true;
@@ -162,8 +178,6 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
-
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
             {
@@ -173,6 +187,8 @@ namespace xadrez
             {
                 vulneravelEnPassant = null;
             }
+
+
         }
 
         public void validarPosicaoDeOrigem(Posicao pos)
---
+++ Udemy/CursoC#/13arquivos/199directory/directory/directory/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+               var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
