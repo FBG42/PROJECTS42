3993272d76a2e0ce206efb10bd5a8a4c90bc1024
fbgrigolo <fbc.grigolo@gmail.com>
2024-03-26 20:36:53 -0300

    Aula de 26/03

---
+++ Udemy/CursoC#/15genericsSetDictionary/213generics214/generics/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            printService _printService = new printService(n);
+
+
+            for (int i = 0; i < n; i++)
+            {
+              int x = int.Parse(Console.ReadLine());
+              _printService.addValue(x);
+            }
+
+            _printService.print();
+            Console.WriteLine("First " + _printService.first());
+        }
+    }
+}
---
+++ Udemy/CursoC#/15genericsSetDictionary/213generics214/generics/printService.cs
@@ -0,0 +1,47 @@
+namespace generics
+{
+    public class printService
+    {
+        private int[] _values;
+        private int _count = 0;
+
+        public printService(int length)
+        {
+            _values = new int[length];
+        }
+
+
+        public void addValue(int value)
+        {
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int first()
+        {
+            if (_values.Length == 0) {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void print()
+        {
+            Console.Write("[");
+
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if (i == _values.Length - 1)
+                {
+                    Console.Write(_values[i]);
+                }
+                else
+                {
+                    Console.Write(_values[i] + ", ");
+                }
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}
