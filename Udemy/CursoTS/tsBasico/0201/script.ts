273caa78317062e0b913881e2c31fe4da345b6d0
fbgrigolo <fbc.grigolo@gmail.com>
2024-07-01 19:08:42 -0300

    exercicio simples do inicio do curso de ts

---
+++ Udemy/CursoTS/tsBasico/0201/script.ts
@@ -0,0 +1,20 @@
+const input = document.querySelector('input');
+
+const total = localStorage.getItem('total');
+if (input && total) {
+	input.value = total;
+	calcularGanho(Number(input.value));
+}
+
+function calcularGanho(value: number) {
+	const p = document.querySelector('p');
+	if (p) p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+}
+
+function totalMudou() {
+	const value = Number(input?.value);
+	localStorage.setItem('total', value.toString());
+	calcularGanho(value);
+}
+
+input?.addEventListener('keyup', totalMudou);
